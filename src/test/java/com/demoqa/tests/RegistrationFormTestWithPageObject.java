6e188e24b244651f3c99e1c7b861830fb4ff825a
RomanG <roma@gmail.com>
2023-08-07 16:48:18 +0300

    fix

--- src/test/java/com/demoqa/tests/RegistrationFormTestWithPageObject.java
+++ src/test/java/com/demoqa/tests/RegistrationFormTestWithPageObject.java
@@ -26,38 +26,17 @@ public class RegistrationFormTestWithPageObject extends BaseTest {
                 .setUserEmail("Boby@Milligan.com")
                 .setBirthDate("25","May", "2000");
         $("#currentAddress").setValue("Anita st.9/4");
-
-        //Выбор пола (поле с выбором из нескольких вариантов)
-        $("#genterWrapper").$(byText("Male")).click();
-
-        //Установка даты в календаре
-//        $("#dateOfBirthInput").click();
-//        $("select.react-datepicker__month-select").selectOption("May");
-//        $("select.react-datepicker__year-select").selectOption("2000");
-//        $(".react-datepicker__day.react-datepicker__day--025").click();
-
-        //Выбор предмета (из выпадающего списка)
         $("#subjectsInput").setValue("History").pressEnter();
-
-        //Выбор хобби
         $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#hobbiesWrapper").$(byText("Music")).click();
-
         $("#uploadPicture").uploadFromClasspath("139207535.jpg");
-
-        //Выбор города
         $("#state").click();
         $("#state").$(byText("Uttar Pradesh")).click();
         $("#city").click();
         $("#city").$(byText("Agra")).click();
-
-        //Отправка результат (нажатие кнопки)
         $("#submit").click();
 
-        //Проверка открытия таблицы
         $(".modal-content").should(appear);
-
-        //Проверка заполнения таблицы
         $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Boby Milligan"));
         $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("Boby@Milligan.com"));
         $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
@@ -68,9 +47,6 @@ public class RegistrationFormTestWithPageObject extends BaseTest {
         $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("139207535.jpg"));
         $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Anita st.9/4"));
         $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("Uttar Pradesh Agra"));
-
-        //Остановка теста на 5 секунд
-//        sleep(10000);
     }
 
         @Test
